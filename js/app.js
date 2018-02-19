2e9cd343f65ba9398ca38977981d02cbfdc19473
PaolaFU <paola_sfu@hotmail.com>
2018-02-19 11:19:01 -0500

    Modificando portafolio

--- js/app.js
+++ js/app.js
@@ -1,72 +1,12 @@
-$("document").ready(function() {
-  //Add interaction to navbar when mouse hover it's elements.
-  $(".nav").on({
-    mouseover: function () {
-      $("li:hover").addClass("active");
-    },
-    mouseout: function () {
-      $("li").removeClass("active");
-    }
+
+
+/*$(document).ready(function() {*/
+  /* prepend menu icon */
+  /*$('#nav').prepend('<div id="menu-icon"></div>');*/
+
+  /* toggle nav */
+  /*$('#menu-icon, #slidenav li').on('click', function() {
+    $('#slidenav').slideToggle();
+    $(this).toggleClass('active');
   });
-  
-  //Smooth scroll   
-  $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
-   
-    // Prevent default anchor click behavior
-    e.preventDefault();
-    
-    // Store hash
-    var hash = this.hash;
-    
-    // Animation
-    $("html, body").animate({
-       scrollTop: $(this.hash).offset().top -50
-    }, 400, function(){
-        
-        // When done, add hash to url
-        // (default click behavior)
-        window.location.hash = hash;
-    });
-  });
-  
-  //Adds Smooth Scrool to arrow down
-  $("#arrow a[href^='#']").on('click', function(e) {
-   
-    // Prevent default anchor click behavior
-    e.preventDefault();
-    
-    // Store hash
-    var hash = this.hash;
-    
-    // Animation
-    $("html, body").animate({
-       scrollTop: $(this.hash).offset().top -50
-    }, 400, function(){
-        
-        // When done, add hash to url
-        // (default click behavior)
-        window.location.hash = hash;
-    });
-  });
-  
-  //Adds Smooth Scrool to Back to Top button
-  $("footer #btt a[href^='#']").on('click', function(e) {
-   
-    // Prevent default anchor click behavior
-    e.preventDefault();
-    
-    // Store hash
-    var hash = this.hash;
-    
-    // Animation
-    $("html, body").animate({
-       scrollTop: $(this.hash).offset().top -50
-    }, 400, function(){
-        
-        // When done, add hash to url
-        // (default click behavior)
-        window.location.hash = hash;
-    });
-  });
-  
-});
+});*/
