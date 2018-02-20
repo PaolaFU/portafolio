c43e5fcbc415dc32854806d8030dc317b1f854e8
PaolaFU <paola_sfu@hotmail.com>
2018-02-20 12:15:07 -0500

    Modificando css

--- js/app.js
+++ js/app.js
@@ -10,3 +10,17 @@
     $(this).toggleClass('active');
   });
 });*/
+$(function () { 
+  $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+});  
+
+// $( window ).scroll(function() {   
+ // if($( window ).scrollTop() > 10){  // scroll down abit and get the action   
+  $(".progress-bar").each(function(){
+    each_bar_width = $(this).attr('aria-valuenow');
+    $(this).width(each_bar_width + '%');
+  });
+       
+ //  }  
+// });
+
